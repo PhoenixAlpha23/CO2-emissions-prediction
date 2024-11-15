b39aa5c335c6732d6d003fe9d1e0b69a6a4eed8e
Aryan Tandon <113663996+PhoenixAlpha23@users.noreply.github.com>
2024-11-16 00:22:38 +0530

    Update data_preprocessing.py
    added carbon emission function based on adjusted weights

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -70,3 +70,24 @@ def get_vehicle_type_distribution(data):
         pd.Series: Count of each vehicle type
     """
     return data['Vehicle Class'].value_counts()
+
+
+def calculate_emissions(initial_weight, distances, num_stops):
+''' weights is in kg
+    distances is in km
+    emission factor is in g/kg weight
+'''
+    total_emissions = 0
+    adjusted_weight = initial_weight
+    
+    for i in range(num_stops):
+        reduction_factor = (adjusted_weight + 1) / (2 * initial_weight)
+        adjusted_weight = adjust_weight_by_stops(adjusted_weight, 1, reduction_factor)
+        leg_emissions = adjusted_weight * distances[i] * emission_factor # leg meaning the current track /distance between 2 points
+        total_emissions += leg_emissions
+        
+    return total_emissions
+
+def adjust_weight_by_stops(weight, num_stops, reduction_factor):
+    adjusted_weight = weight * (1 - reduction_factor * num_stops)
+    return adjusted_weight
